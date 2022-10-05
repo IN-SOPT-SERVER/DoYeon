f5abd6a24d5680e4cc2ee070cd5d221fd4e5dfa2
doyeoo <ehdusla013@gmail.com>
2022-10-05 16:36:34 +0900

    basic: 타입과 함수

---
+++ week1/basic/functions.js
@@ -0,0 +1,20 @@
+// 함수 선언식
+// function hello(name) {
+//     console.log(`${name} 안녕`);
+// }
+
+// hello('도연');
+
+// 함수 표현식
+// const sum = (a, b) => {
+//     const result = a+b;
+//     console.log(result);
+//     return result; //return a+b;
+// }
+
+// const sum = (a, b) => a+b; 이렇게 쓸 수도 있지만 복잡한 계산의 경우 지양할 것
+
+// sum(1,2);
+
+const test = 'textext'
+console.log(typeof test)
